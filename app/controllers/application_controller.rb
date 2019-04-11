da43518741facb95f027bf4e2868556b445f4c39
vantri0964 <vantri27051998@gmail.com>
2019-04-11 23:28:24 +0700

    heroku-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-	def 
-		
-	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def new
   end
+  def show
+  	@user=User1.all
+  end
 end
