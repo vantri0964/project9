61275c62440013aa219c1855eea2bca94e1d9079
vantri0964 <vantri27051998@gmail.com>
2019-04-13 14:27:10 +0700

    fix database

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :users
-  root 'users#new'
+  root 'users#show'
   get '/show', to:'users#show'
 end
