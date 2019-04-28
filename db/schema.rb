3e0af2e24b881ec7e820d9d9801d6cd96aff2028
vantri0964 <vantri27051998@gmail.com>
2019-04-29 00:02:03 +0700

    chapter 12

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_27_084415) do
+ActiveRecord::Schema.define(version: 2019_04_28_093511) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2019_04_27_084415) do
     t.string "activation_digest"
     t.boolean "activated"
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
